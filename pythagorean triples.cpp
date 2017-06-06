a8027ccfab876cff18e35062dc1a5b7b37091269
KacperWeiss <KacperWeiss@users.noreply.github.com>
2017-06-06 17:41:33 +0200

    Update pythagorean triples.cpp

--- pythagorean triples.cpp
+++ pythagorean triples.cpp
@@ -4,9 +4,15 @@
 #include <fstream>
 #include <cstring>
 
+/*!
+ * \file pythagorean triples.cpp
+ *
+ * File includes short program that generates pithagorean triples based on 2 integers.
+ */
+
 using namespace std;
 
-void menu()
+void menu() // Short function that displays menu
 {
   cout << "n - shows and adds a new pythagorean triple to file\n"
        << "r - removes last file and replace it with new one\n"
@@ -18,7 +24,7 @@ void menu()
 int main()
 {
   fstream _file;
-  string name = "List of pythagorean triples";
+  string name = "List of pythagorean triples";	//default name for a file with resaults
   int v,u;
   char choice;
   int pythagoreanTriple[3];
@@ -32,7 +38,7 @@ int main()
     cin >> choice;
     if(strchr("nrsmq", choice))
     {
-      if (choice == 'n')
+      if (choice == 'n') // Creates new pythagorean triple
       {
         cout << "Give me your first number!\n";
 	cin >> v;
@@ -51,17 +57,17 @@ int main()
 	      << pythagoreanTriple[1] << "," << pythagoreanTriple[2] << ")\n";
 	_file.close();
       }
-      else if (choice == 'r')
+      else if (choice == 'r') // removes last file with resaults
       {
-	system("rm -f *.dat");
+	system("rm -f *.dat"); // removes last file with resaults
 	cout << "New name: ";
 	cin >> name;
-	_file.open(name+string(".dat"), ios::app);
+	_file.open(name+string(".dat"), ios::app); // creates new file for resaults
 	if (!_file.is_open())
 	  cerr << "Opening of file \"" << name << "\" failed" << endl;
 	_file.close();
       }
-      else if (choice == 's')
+      else if (choice == 's') // creates a folder (if it's not already existing) for saved resaults
       {
 	system("mkdir -p saved\n cp *.dat saved");
 	system("rm -f *.dat");
@@ -82,7 +88,7 @@ int main()
 	break;
       }
     }
-    else
+    else // if someone chose non-existing option it will show error allert and menu 
     {
       cin.clear();
       cin.ignore(10000, '\n');
